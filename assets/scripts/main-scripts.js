856477ee849d3fab915f666ddc0d4781f8e594bc
Nathan Ryan <rockymountainshredder@gmail.com>
2022-02-01 20:57:35 -0700

    Created rudimentary working version of console rock paper scissors game.

--- assets/scripts/main-scripts.js
+++ assets/scripts/main-scripts.js
@@ -1,10 +1,39 @@
 
-/* JavaScript code for Rock Paper Scissors Game */
+/* Rudimentary Console Based Rock Paper Scissors Game */
+
+playGame();
+
+function playGame() {
+
+    const playerSelection = userPlay();
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+}
+
+
+// Play a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+
+    // Compare the two entries and signify win, loss or tie.
+    const gameResult = evaluateMatch(playerSelection, computerSelection);
+    console.log(gameResult);
+}
+
+// Prompt the user to enter either: Rock, Paper or Scissors
+function userPlay() {
+    
+    const userChoice = prompt(`Enter your selection: Rock, Paper or Scissors.`);
+    
+    // Validation needed to ensure only of the three values are entered, nothing else. 
+    // (userChoice != 'Rock' || userChoice != 'Paper' || userChoice != 'Scissors') 
+
+    return userChoice;
+}
 
 // Randomly return either: Rock, Paper or Scissors
 function computerPlay() {
 
-    // ** Refactor function **
+    // ** Refactor function ** //
     const rock = 0;
     const paper = 1;
     const scissors = 2;
@@ -28,5 +57,83 @@ function computerPlay() {
 
         default:
             break;
+    }
+}
+
+// A function to compare entries, determine result
+function evaluateMatch(playerSelection, computerSelection) {
+
+    // Rules of Rock, Paper, Scissors
+    // Rock > Scissors
+    // Paper > Rock
+    // Scissors > Paper
+    // Rock = Rock
+
+    // ** Refactor Function ** // 
+
+    if(playerSelection == 'Rock') {
+
+        switch (computerSelection) {
+
+            case 'Rock':
+                return `Tie game: Player(${playerSelection}) = Computer(${computerSelection})`;               
+                break;
+        
+            case 'Paper':
+                return `You lose: Player(${playerSelection}) < Computer(${computerSelection})`;
+                break;
+
+            case 'Scissors':
+                alert(`Congratulations! You win: Player(${playerSelection}) > Computer(${computerSelection})`);
+                return `You win: Player(${playerSelection}) > Computer(${computerSelection})`;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    if(playerSelection == 'Paper') {
+
+        switch (computerSelection) {
+
+            case 'Paper':
+                return `Tie game: Player(${playerSelection}) = Computer(${computerSelection})`;               
+                break;
+        
+            case 'Scissors':
+                return `You lose: Player(${playerSelection}) < Computer(${computerSelection})`;
+                break;
+
+            case 'Rock':
+                alert(`Congratulations! You win: Player(${playerSelection}) > Computer(${computerSelection})`);
+                return `You win: Player(${playerSelection}) > Computer(${computerSelection})`;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    if(playerSelection == 'Scissors') {
+
+        switch (computerSelection) {
+
+            case 'Scissors':
+                return `Tie game: Player(${playerSelection}) = Computer(${computerSelection})`;               
+                break;
+        
+            case 'Rock':
+                return `You lose: Player(${playerSelection}) < Computer(${computerSelection})`;
+                break;
+
+            case 'Paper':
+                alert(`Congratulations! You win: Player(${playerSelection}) > Computer(${computerSelection})`);
+                return `You win: Player(${playerSelection}) > Computer(${computerSelection})`;
+                break;
+
+            default:
+                break;
+        }
     }
 }

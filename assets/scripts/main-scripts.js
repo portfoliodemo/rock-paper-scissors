b914a9883f3c866933c81df0eb301005bfd6aa96
Nathan Ryan <rockymountainshredder@gmail.com>
2022-02-18 23:11:21 -0700

    Template literal and data attribute experimentation

--- assets/scripts/main-scripts.js
+++ assets/scripts/main-scripts.js
@@ -1,3 +1,16 @@
 
-/* Rudimentary Console Based Rock Paper Scissors Game */
+/* Rock Paper Scissors Game */
 
+const rock = document.querySelector(`figure[data-selection="rock"]`);
+//console.log(rock);
+rock.addEventListener('click', playGame);
+
+function playGame() {
+    const confirm = window.confirm(`You Chose Rock! \nConfirm Y/N?`);
+    if(confirm) {
+        alert(`Let the Games Begin!`);
+    }
+    else {
+        alert(`You have cancelled your selection of Rock. \nPlease choose again.`);
+    }
+}
